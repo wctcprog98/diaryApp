6581c443fa2ffd2313237de81e9a747c15536c6f
root <chinenyediei@gmail.com>
2020-10-12 16:18:33 -0500

    not able to create multiple message boxes yet

--- js/index.js
+++ js/index.js
@@ -6,17 +6,20 @@ let entriesSection = document.getElementById('entries')
 //variable to store entries
 let textbox = document.getElementsByClassName('entry-textbox')
 
-function addEntryToDom(event) {
-    event.preventDefault();
-      //clear form
-    //   textbox[0].value = ''
+
+
+
+function addEntryToDom(e) {
+    e.preventDefault()
     const entryDiv = document.createElement('div')
     entryDiv.className ='single-entry'
     entryDiv.innerText = textbox[0].value
+    console.log(textbox[0].value)
     entriesSection.appendChild(entryDiv)
+    // diaryArray.push()
     textbox[0].value = ''
 }
-
+    
 entryForm.addEventListener('submit', addEntryToDom)
 const submitBtn = document.getElementById('submit')
 
@@ -24,6 +27,15 @@ submitBtn.addEventListener('click', function () {
     console.log('clicked')
     entriesSection.style.padding = '2em'
     entriesSection.style.maxWidth = '20em'
-    entriesSection.style.marginTop = '1em' 
+    entriesSection.style.marginTop = '1em'
+    
 })
+
+const entryDiv = document.createElement('div')
+entryDiv.className ='single-entry'
+entryDiv.innerText = textbox[0].value
+console.log(textbox[0].value)
+entriesSection.appendChild(entryDiv)
+// diaryArray.push()
+textbox[0].value = ''
 
